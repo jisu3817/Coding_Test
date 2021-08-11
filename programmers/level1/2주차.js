2890d8332c091d1bee2199fbe4551024208d378e
김지수 <kimjisu3817@gmail.com>
2021-08-11 23:25:39 +0900

    프로그래머스 level1 풀이

---
+++ programmers/level1/2주차.js
@@ -0,0 +1,44 @@
+function solution(scores) {
+    let answer = '';
+    let grades = [];
+    let grade = ''
+    
+    for (let i = 0; i < scores.length; i++) {
+        let sum = 0;
+        let max = -1; 
+        let min = 101;
+        let avg = 0;
+        let len = scores.length;
+        
+        for (let j = 0; j < scores[i].length; j++) {
+            max = Math.max(max,scores[j][i]);
+            min = Math.min(min,scores[j][i]);
+            sum += scores[j][i];
+        };
+        
+        let maxCount = 0;
+        let minCount = 0;
+        
+        for(let j = 0; j < scores[i].length; j++) {
+            if (scores[j][i] === max) maxCount++;
+            else if (scores[j][i] === min) minCount++;
+        };
+        
+        if ((maxCount === 1 && max === scores[i][i]) || (minCount === 1 && min === scores[i][i])) {
+            sum -= scores[i][i];
+            avg = sum / (len - 1);
+        }
+        else avg = sum / len;
+    
+        if (avg >= 90) grade = 'A'
+        else if (avg < 90 && avg >= 80) grade = 'B'
+        else if (avg < 80 && avg >= 70) grade = 'C'
+        else if (avg < 70 && avg >= 50) grade = 'D'
+        else grade = 'F';
+        
+        grades[i] = grade;
+        answer += grades[i];
+    }
+    return answer;
+}
+
