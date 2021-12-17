ee7403c46d084091f9decbff491cf7f4c2824b7b
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-12-17 11:10:43 +0900

    Add(programmers/level2): 뉴스 클러스터링 풀이

---
+++ programmers/level2/뉴스-클러스터링.js
@@ -0,0 +1,28 @@
+function solution(str1, str2) {
+    const answer = '';
+    const regeEx =  /^[a-z]*$/;
+    
+    let multipleSet1 = str1.toLowerCase().split('').map((str, i, str1) => str + str1[i + 1]);
+    let multipleSet2 = str2.toLowerCase().split('').map((str, i, str2) => str + str2[i + 1]);
+    
+    multipleSet1.pop();
+    multipleSet2.pop();
+    
+    multipleSet1 = multipleSet1.filter(el => regeEx.test(el));
+    multipleSet2 = multipleSet2.filter(el => regeEx.test(el));
+    
+    const set = new Set([...multipleSet1, ...multipleSet2]);
+    let union = 0;
+    let intersection = 0;
+    
+    set.forEach(item => {
+        const has1 = multipleSet1.filter(el => el === item).length;
+        const has2 = multipleSet2.filter(el => el === item).length;
+        
+        union += Math.max(has1, has2);
+        intersection += Math.min(has1, has2);
+    });
+    
+    return union === 0 ? 65536 : Math.floor(intersection / union * 65536);
+}
+
