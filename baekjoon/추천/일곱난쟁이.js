5a4ba869c5e88fd5edc62aa56d4116ca9d35b995
김지수 (Kim jisoo) <79014269+jisu3817@users.noreply.github.com>
2022-11-11 10:08:29 +0900

    Update 일곱난쟁이.js

--- baekjoon/추천/일곱난쟁이.js
+++ baekjoon/추천/일곱난쟁이.js
@@ -11,11 +11,11 @@ const sortedInput = inputArr.sort((a, b) => a - b);
 
 const excessHeightSum = sortedInput.reduce((acc, cur) => acc + cur) - 100;
 
-let check1, check2;
 for (let i = 0; i < sortedInput.length - 1; i += 1) {
   for (let j = i + 1; j < sortedInput.length; j += 1) {
     if (sortedInput[i] + sortedInput[j] === excessHeightSum) {
-      [(check1, check2)] = [i, j];
+      sortedInput.splice(j);
+      sortedInput.splice(i);
       break;
     }
   }
