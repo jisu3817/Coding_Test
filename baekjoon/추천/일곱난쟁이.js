57ece20b6f8650214f5bb5c0a3cc264cfb89ace8
김지수 <kimjisu3817@gmail.com>
2022-11-15 00:33:23 +0900

    baekjoon/추천: 최대공약수와 최소공배수 풀이

--- baekjoon/추천/일곱난쟁이.js
+++ baekjoon/추천/일곱난쟁이.js
@@ -11,14 +11,16 @@ const sortedInput = inputArr.sort((a, b) => a - b);
 
 const excessHeightSum = sortedInput.reduce((acc, cur) => acc + cur) - 100;
 
+let check1, check2;
 for (let i = 0; i < sortedInput.length - 1; i += 1) {
   for (let j = i + 1; j < sortedInput.length; j += 1) {
     if (sortedInput[i] + sortedInput[j] === excessHeightSum) {
-      sortedInput.splice(j);
-      sortedInput.splice(i);
+      [check1, check2] = [i, j];
       break;
     }
   }
 }
 
-console.log(sortedInput.join("\n"));
+for (let i = 0; i < 9; i += 1) {
+  if (i !== check1 && i !== check2) console.log(result[i].toString());
+}
